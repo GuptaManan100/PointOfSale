b26926b04e6748fd533af45217be87aaeb2ae56a
GuptaManan100 <guptamanan100@gmail.com>
2020-03-03 16:30:12 +0530

    Server Side Half Done

--- server.c
+++ server.c
@@ -4,19 +4,44 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
+#include <signal.h> 
+#include <pthread.h>
 #include "server.h"
+#include "lock.h"
+
+//Global Variables
+pthread_t tid;
+int serverSocket, connectionSocket;
+struct sockaddr_in serverAddress;
+int opt = 1;
+int addressLen = sizeof(serverAddress);
+
+void* serverThread(void *csocket)
+{
+	//serverCode
+	int total = 0;
+	int socket = * (int *)csocket;
+
+	return NULL;
+}
+
+void sigintHandler(int sig_num) 
+{ 
+    signal(SIGINT, sigintHandler); 
+    close(serverSocket);
+    printf("\nExited Gracefully\n");
+    exit(0);
+} 
 
 int main(int argc, char const *argv[]) 
 {
+	signal(SIGINT, sigintHandler); 
+
 	if (argc != 2 || atoi(argv[1])<=0 )
 	{
 		printf("Usage:- ./server <port number> \n");
 		exit(FAILURE);
 	}
-
-	int serverSocket, connectionSocket;
-	struct sockaddr_in serverAddress;
-	int opt = 1;
 
 	if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) <= 0) 
     { 
@@ -41,9 +66,22 @@ int main(int argc, char const *argv[])
         exit(FAILURE); 
     } 
 
-    if (listen(serverSocket, 5) < 0) 
+    if (listen(serverSocket, BACKLOG) < 0) 
     { 
         printf("Error in Listen\n"); 
         exit(FAILURE); 
     } 
+
+    while(1)
+    {
+    	connectionSocket = accept(serverSocket, (struct sockaddr *)&serverAddress, (socklen_t*)(&addressLen));
+    	if(connectionSocket<0)
+    	{
+    		printf("Connection Error\n");
+    	}
+    	if(pthread_create(&tid, NULL, &serverThread, &connectionSocket))
+    	{
+    		printf("Error creating thread\n");
+    	}
+    }
 }
--- server.h
+++ server.h
@@ -1,4 +1,6 @@
 #ifndef SERVER_HEAD
 #define SERVER_HEAD
 	#define FAILURE 1
+	#define BACKLOG 5
+	#define NUM_THREADS 10
 #endif
